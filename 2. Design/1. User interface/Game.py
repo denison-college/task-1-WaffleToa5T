0013f420a83aadb90a0de697716b9e8563c4aa80
WaffleToa5T <tysonwilson86@gmail.com>
2026-04-23 14:05:57 +1000

    1

--- 2. Design/1. User interface/Game.py
+++ 2. Design/1. User interface/Game.py
@@ -2,12 +2,10 @@ import os
 def play():
     print("Press any key to start")
     
-while x == True:
+while 'x' == True:
     print("Welcome to Snake Game")
     menu = input("What would you like to do? ")
     if menu == "start":
         play()
     elif menu == "quit":
         exit()
-    else:
-        os.system('cls')
